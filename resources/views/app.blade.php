0d89de8ab789c260b6d2989b272ff7a5f01f6c5f
jugol-kumar <jk23717933@gmail.com>
2024-09-01 16:50:34 +0600

    update side favicon and title

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,7 +4,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Sky Dream | {{ env('APP_NAME')}}</title>
+    <title>Forest Painters</title>
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('/apple-touch-icon.png') }}">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('/favicon-32x32.png') }}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('/favicon-16x16.png') }}">
+
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
